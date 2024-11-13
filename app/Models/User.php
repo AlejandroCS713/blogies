a23eb9a54dd34faf10dcced5d0695f1b5252ecd4
AlejandroCS713 <alejandro2003carbonell@gmail.com>
2024-11-13 16:23:17 +0100

    Commit Ejercicio6

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(Post::class);
     }
+
+    public function role()
+    {
+        return $this->hasOne(Role::class); 
+    }
 }
