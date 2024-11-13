d79b291a3daa2b675bcc5e453a39ec76d7be6e4c
AlejandroCS713 <alejandro2003carbonell@gmail.com>
2024-11-13 15:42:26 +0100

    Commit Ejercicio2

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -16,9 +17,16 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $title = $this->faker->sentence();
+        $body = $this->faker->paragraphs(3, true);
+        $readingTime = ceil(str_word_count($body) / 200);
         return [
-            'title' => $this->faker->sentence(),
-            'body' => $this->faker->paragraph(),
+            'title' => $title,
+            'body' => $body,
+            'summary' => substr($body, 0, 50),
+            'slug' => Str::slug($title),
+            'status' => $this->faker->randomElement(['published', 'draft', 'archived', 'pending']),
+            'reading_time' => $readingTime,
             'published_at' => random_int(0, 2)
                 ? $this->faker->dateTimeBetween('-1 month', '+1 months')
                 : null,
